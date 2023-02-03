e8b4b8ae85def818cb830900a224c30bd97cbb7e
Ruslan91111 <ruslan_ru911@mail.ru>
2023-02-03 09:30:15 +0500

    APITestCase - Done

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -5,17 +5,22 @@ from store.models import Book
 from store.serializers import BooksSerializer
 
 
-# Тестируем API.
+# Тестируем API - запросы.
 class BooksApiTestCase(APITestCase):
+    """Test work of serialazer"""
     def test_get(self):
-        # Создаем 2 тестовых экземпляра книг.
+        # Создаем 2 тестовых экземпляра книг в тестовой БД.
         book_1 = Book.objects.create(name='Test book 1', price=25)
         book_2 = Book.objects.create(name='Test book 2', price=55)
+
+        # DRF reverse создает нужный нам url. Для получения списка при помощи ViewSet 'book-list',
+        # также можно 'book-detail'.
         url = reverse('book-list')
+
         # self.client предоставляется APITestCase, по сути является имитацией клиента, в том числе браузера.
         response = self.client.get(url)
 
-        # many=True означает, что сериализовать нужно все объекты.
+        # many=True означает, что сериализовать нужно оба объекта. Переменной присвоить данные после сериализации.
         serializer_data = BooksSerializer([book_1, book_2], many=True).data
 
         # Перед тем как сравнивать сами данные мы должны убедиться, что код возврата
