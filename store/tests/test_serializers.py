b232dc6ed538b2d6e102a5670bb9047148c722f5
Ruslan91111 <ruslan_ru911@mail.ru>
2023-02-03 10:10:41 +0500

    testing1

--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -8,11 +8,7 @@ from store.serializers import BooksSerializer
 # В var expected_data, то что ожидаем увидеть от работы сериализатора.
 # Сравниваем обе переменных
 class BookSerializerTestCase(TestCase):
-<<<<<<< HEAD
     """Test work of serializer"""
-=======
-    """Test work of serializer"""
->>>>>>> testing2
     def test_ok(self):
         book_1 = Book.objects.create(name='Test book 1', price=25)
         book_2 = Book.objects.create(name='Test book 2', price=55)
