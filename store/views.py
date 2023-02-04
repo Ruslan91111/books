a509e125d8cacdf6d00a2b0666bd8f61d2fa3c45
Ruslan91111 <ruslan_ru911@mail.ru>
2023-02-04 11:20:57 +0500

    OrderingFilter

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BooksSerializer
@@ -10,11 +10,12 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     # Настраиваем filter, search, ordering.
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     # Указываем поле, по которому хотим отфильтровать.
     filterset_fields = ['price']
     # Поля для поиска. Поиск использовать, для поиска по двум и более полям, иначе это просто фильтр.
     search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
 
 
 
