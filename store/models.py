7944c9670d41dcda25caf8de3584a649f1c53e63
Ruslan91111 <ruslan_ru911@mail.ru>
2023-02-04 11:14:40 +0500

    search

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ class Book(models.Model):
     # DecimalField типа float, только фиксируем количество цифр.
     # В данном примере 5 цифр до запятой, 2 после.
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    author_name = models.CharField(max_length=255)
 
     # Переопределение магического метода - строкового представления экземпляра класса.
     def __str__(self):
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BooksSerializer
@@ -8,10 +9,12 @@ from .serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-    # Настраиваем фильтр джанго.
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    # Настраиваем filter, search, ordering.
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter]
     # Указываем поле, по которому хотим отфильтровать.
     filterset_fields = ['price']
+    # Поля для поиска. Поиск использовать, для поиска по двум и более полям, иначе это просто фильтр.
+    search_fields = ['name', 'author_name']
 
 
 
